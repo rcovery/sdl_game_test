7548c9f949257d959fd3ca87c94caf30fe8f7ab6
Ryan <ryancpereira@protonmail.com>
2023-03-07 20:36:03 -0300

    Adding base files

---
+++ src/constants.h
@@ -0,0 +1,2 @@
+#define GAYME_WIDTH 800
+#define GAYME_HEIGHT 600
--- src/main.c
+++ src/main.c
@@ -1,37 +1,39 @@
-#include <stdio.h>
 #include <SDL2/SDL.h>
+#include <stdio.h>
 
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
+#include "./constants.h"
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
 
 int init_window(void) {
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        return EXIT_FAILURE;
-    }
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    return EXIT_FAILURE;
+  }
 
-    window = SDL_CreateWindow(
-        "Gayme",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
-        0
-    );
+  window = SDL_CreateWindow(
+    "Gayme",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    GAYME_WIDTH,
+    GAYME_HEIGHT,
+    0
+  );
 
-    if (!window) {
-        return EXIT_FAILURE;
-    }
+  if (!window) {
+    return EXIT_FAILURE;
+  }
 
-    renderer = SDL_CreateRenderer(window,);
+  renderer = SDL_CreateRenderer(window, -1, 0);
 
-    if (!renderer) {
-        return EXIT_FAILURE;
-    }
+  if (!renderer) {
+    return EXIT_FAILURE;
+  }
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
 
-int main(int args, char* argc[]) {
-    init_window();
-    return 0;
+int main(int args, char *argc[]) {
+  init_window();
+  return 0;
 }
